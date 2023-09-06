388cc8251a7d71ffb84d1d6e8c55e803d1c59f91
safethecode <iam@sonjim.in>
2023-09-07 04:15:48 +0900

    feat(styles/keyframes): 포지션 키프레임 추가

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,2 +1,4 @@
 export * from './globalStyle.css';
 export * from './globalFontsStyle';
+export * from './commonStyle.css';
+export * from './keyframes.css';
---
+++ src/styles/keyframes.css.ts
@@ -0,0 +1,6 @@
+import { keyframes } from '@vanilla-extract/css';
+
+export const position = keyframes({
+  '50%': { transform: 'translate(0, -16px)' },
+  '100%': { transform: 'translate(0, 0)' },
+});
